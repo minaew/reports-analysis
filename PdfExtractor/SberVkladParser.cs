97809e72c5752abaf08cd4438b46d727ee212c05
Minaev Alexey <manai.mina@gmail.com>
2022-04-18 23:03:18 +0300

    Fixed behavior for missing header

--- PdfExtractor/SberVkladParser.cs
+++ PdfExtractor/SberVkladParser.cs
@@ -30,31 +30,38 @@ namespace PdfExtractor
                 {
                     var words = page.GetWords().ToList();
 
-                    var dateTitle = PdfHelper.GetRect(words, "ДАТА", "ОПЕРАЦИИ").Value;
+                    var dateRect = PdfHelper.GetRect(words, "ДАТА", "ОПЕРАЦИИ");
+                    var descriptionRect = PdfHelper.GetRect(words, "НАИМЕНОВАНИЕ", "ОПЕРАЦИИ");
+                    var amountRect = PdfHelper.GetRect(words, "СУММА", "ОПЕРАЦИИ");
+                    if (!dateRect.HasValue || !descriptionRect.HasValue || !amountRect.HasValue)
+                    {
+                        continue;
+                    }
 
-                    var dates = words.Where(w => w.BoundingBox.Top < dateTitle.Top &&
-                                                 w.BoundingBox.Left >= dateTitle.Left &&
-                                                 w.BoundingBox.Right <= dateTitle.Right)
+                    var dateHeader = dateRect.Value;
+                    var descriptionHeader = descriptionRect.Value;
+                    var amountHeader = amountRect.Value;
+                    amountHeader.Right += 2; // СУММА ОПЕРАЦИИ ^3
+
+                    var dates = words.Where(w => w.BoundingBox.Top < dateHeader.Top &&
+                                                 w.BoundingBox.Left >= dateHeader.Left &&
+                                                 w.BoundingBox.Right <= dateHeader.Right)
                                      .ToList();
-
-                    var descriptionTitle = PdfHelper.GetRect(words, "НАИМЕНОВАНИЕ", "ОПЕРАЦИИ").Value;
-                    var amountTitle = PdfHelper.GetRect(words, "СУММА", "ОПЕРАЦИИ").Value;
-                    amountTitle.Right += 2; // СУММА ОПЕРАЦИИ ^3
 
                     foreach (var date in dates)
                     {
                         var dateToken = date.Text;
                         if (!_dateRegex.IsMatch(dateToken)) continue;
 
-                        var descriptionWords = words.Where(w => w.BoundingBox.Left >= descriptionTitle.Left &&
-                                                                w.BoundingBox.Right <= descriptionTitle.Right &&
+                        var descriptionWords = words.Where(w => w.BoundingBox.Left >= descriptionHeader.Left &&
+                                                                w.BoundingBox.Right <= descriptionHeader.Right &&
                                                                 w.BoundingBox.Top >= date.BoundingBox.Centroid.Y &&
                                                                 w.BoundingBox.Bottom <= date.BoundingBox.Centroid.Y)
                                                     .ToList();
                         var description = string.Join(" ", descriptionWords.Select(w => w.Text)); // todo: order?
 
-                        var amountWords = words.Where(w => w.BoundingBox.Left >= amountTitle.Left &&
-                                                           w.BoundingBox.Right <= amountTitle.Right &&
+                        var amountWords = words.Where(w => w.BoundingBox.Left >= amountHeader.Left &&
+                                                           w.BoundingBox.Right <= amountHeader.Right &&
                                                            w.BoundingBox.Top >= date.BoundingBox.Centroid.Y &&
                                                            w.BoundingBox.Bottom <= date.BoundingBox.Centroid.Y)
                                                 .ToList();
